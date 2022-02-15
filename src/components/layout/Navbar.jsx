6e6847cc08bfced5e494e5db4fce5ddf3ca2c21f
yvonneshui <yvonne.shui126@gmail.com>
2022-02-15 12:07:08 -0500

    feature: routes added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,8 +13,7 @@ function Navbar({title}) {
 			</Link>
 		</div>
 
-		<div className="flex
-		-1 px-1 mx-2">
+		<div className="flex-1 px-1 mx-2">
 			<div className="flex justify-end">
 				<Link to='/' className='btn btn-ghost btn-sm rounded-btn'>Home
 				</Link>
