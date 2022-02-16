3a8ccda34b797a087c970e0976b7a77543c6625e
yvonneshui <yvonne.shui126@gmail.com>
2022-02-16 14:31:02 -0500

    feature: use context on UserResult

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,20 +1,14 @@
-import {useEffect,useState} from 'react'
+import {useEffect,useContext,} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-	const [users,setUsers]=useState([])//changes user data
-	const[loading,setLoading]=useState(true)
+const {users,loading,fetchUsers}=useContext(GithubContext)
+
 	useEffect(()=>{
 		fetchUsers()
 	},[])//runs every render
-
-	const fetchUsers=async()=>{
-		const res=await fetch(`http://api.github.com/users`)
-		const data= await res.json()
-		setUsers(data);
-		setLoading(false)
-	}
 
 	return loading?<Spinner />:(
 		<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
