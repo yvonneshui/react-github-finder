0ee25fefe6677b20f27b46a666d88ce6ff9ce071
yvonneshui <yvonne.shui126@gmail.com>
2022-02-16 11:02:27 -0500

    feature: spinner added when loading

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,14 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+	return (
+		<div className="w-100 mt20">
+			<img width={180} 
+			className='text-center mx-auto' 
+			src={spinner} 
+			alt='Loading...'/>
+		</div>
+	)
+}
+
+export default Spinner
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,5 @@
 import {useEffect,useState} from 'react'
-
+import Spinner from '../layout/Spinner'
 function UserResults() {
 	const [users,setUsers]=useState([])//changes user data
 	const[loading,setLoading]=useState(true)
@@ -17,7 +17,7 @@ function UserResults() {
 		setLoading(false)
 	}
 
-	return loading?<h1>loading</h1>:(
+	return loading?<Spinner />:(
 		<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
 			{users.map((user,key)=>(
 				<h3>{user.login}</h3>
