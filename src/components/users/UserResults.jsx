8266b406594fcbb501ed58734b1ed7e4fb8b47e6
yvonneshui <yvonne.shui126@gmail.com>
2022-02-16 15:06:39 -0500

    feature: search user component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,12 @@
-import {useEffect,useContext,} from 'react'
+import {useContext,} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-const {users,loading,fetchUsers}=useContext(GithubContext)
+const {users,loading}=useContext(GithubContext)
 
-	useEffect(()=>{
-		fetchUsers()
-	},[])//runs every render
+
 
 	return loading?<Spinner />:(
 		<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
 function Home() {
 	return (
 		<div>
 			{/*search components will be here*/}
+			<UserSearch />
 			<UserResults />
 		</div>
 	)
